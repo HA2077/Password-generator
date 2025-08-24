57596b28aaf060b6a7088c55849061ff8f5a7ba6
Hassan Ahmed <hassanstar2007@gmail.com>
2025-08-24 22:37:06 +0300

    Making the code more clear + using random.choice()

--- Program.py
+++ Program.py
@@ -1,16 +1,12 @@
 import random
 
 print ("The password generator")
-lowercase1 = chr(random.randint(97, 122))
-lowercase2 = chr(random.randint(97, 122))
-uppercase1 = chr(random.randint(65, 90))
-uppercase2 = chr(random.randint(65, 90))
-number1 = chr(random.randint(48, 57))
-number2 = chr(random.randint(48, 57))
-symbol1 = chr(random.randint(33, 47))
-symbol2 = chr(random.randint(33, 47))
+Lowercase = "abcdefghijklmnopqrstuvwxyz"
+Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+Symbol = "!@#$%^&*()_+-=[]{}|;:,.<>?/~"
+Number = "0123456789"
 
-password_chars = [number2, symbol1, uppercase1, uppercase2, number1, lowercase1, lowercase2, symbol2]
+password_chars = [random.choice(Number), random.choice(Symbol), random.choice(Uppercase), random.choice(Uppercase), random.choice(Number), random.choice(Lowercase), random.choice(Lowercase), random.choice(Symbol)]
 random.shuffle(password_chars)
 password = ''.join(password_chars)
 

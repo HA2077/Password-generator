19d90ad214d34ddb4f4c455898f217baa595e822
Hassan Ahmed <hassanstar2007@gmail.com>
2025-09-02 20:11:37 +0300

    Add password improvement functionality to enhance generated passwords

--- Program.py
+++ Program.py
@@ -18,10 +18,43 @@ def Passgen(Length, UseLowercase, UseUppercase, UseSymbol, UseNumber, Lowercase,
     random.shuffle(password_chars)
     return ''.join(password_chars)
 
+def Improve_password(password, use_lowercase, use_uppercase, use_symbol, use_number, Lowercase, Uppercase, Symbol, Number):
+    password_list = list(password)
+    has_lowercase = any(c.islower() for c in password_list)
+    has_uppercase = any(c.isupper() for c in password_list)
+    has_symbol = any(not c.isalnum() for c in password_list)
+    has_number = any(c.isdigit() for c in password_list)
+    
+    replacements = []
+    if use_lowercase and not has_lowercase: replacements.append(random.choice(Lowercase))
+    if use_uppercase and not has_uppercase: replacements.append(random.choice(Uppercase))
+    if use_symbol and not has_symbol: replacements.append(random.choice(Symbol))
+    if use_number and not has_number: replacements.append(random.choice(Number))
+
+    for i, replacement in enumerate(replacements):
+        if i < len(password_list):
+            password_list[i] = replacement
+    
+    available_chars = ""
+    if use_lowercase: available_chars += Lowercase
+    if use_uppercase: available_chars += Uppercase
+    if use_symbol: available_chars += Symbol
+    if use_number: available_chars += Number
+
+    seen = set()
+    for i in range(len(password_list)):
+        if password_list[i] in seen:
+            for char in available_chars:
+                if char not in seen:
+                    password_list[i] = char
+                    break
+        seen.add(password_list[i])
+    
+    return ''.join(password_list)
+
 print ("The password generator")
 while True:
     Length = int(input("Enter the length of the password (min 8 characters & max 32 characters): "))
-    UseLowercase, UseUppercase, UseSymbol, UseNumber = User_preferences()
     Lowercase = "abcdefghijklmnopqrstuvwxyz"
     Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     Symbol = "!@#$%^&*()_+-=[]{}|;:,.<>?/~"
@@ -33,7 +66,9 @@ while True:
     elif Length > 32:
         print("Password length should not exceed 32 characters.")
     else:
+        UseLowercase, UseUppercase, UseSymbol, UseNumber = User_preferences()
         password = Passgen(Length, Lowercase, Uppercase, Symbol, Number, UseLowercase, UseUppercase, UseSymbol, UseNumber)
+        password = Improve_password(password, UseLowercase, UseUppercase, UseSymbol, UseNumber, Lowercase, Uppercase, Symbol, Number)
         print("Generated Password: " + password)
 
     print("Do you want to generate another password? (yes/no)")

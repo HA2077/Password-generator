bdc814641d3430f1b475285f2b3c96b413ac6d2c
Hassan Ahmed <hassanstar2007@gmail.com>
2025-09-23 18:33:41 +0300

    Removed the "Improve_password" function and used a better approch in the "Passgen" function

--- Program.py
+++ Program.py
@@ -12,45 +12,26 @@ def User_preferences():
             return UseLowercase, UseUppercase, UseSymbol, UseNumber
 
 def Passgen(Length, UseLowercase, UseUppercase, UseSymbol, UseNumber, Lowercase, Uppercase, Symbol, Number):
-    password_chars = []
-    for i in range(Length):
-        password_chars.append(secrets.choice((UseLowercase*Lowercase) + (UseUppercase*Uppercase) + (UseSymbol*Symbol) + (UseNumber*Number)))
-    secrets.SystemRandom().shuffle(password_chars)
-    return ''.join(password_chars)
-
-def Improve_password(password, use_lowercase, use_uppercase, use_symbol, use_number, Lowercase, Uppercase, Symbol, Number):
-    password_list = list(password)
-    has_lowercase = any(c.islower() for c in password_list)
-    has_uppercase = any(c.isupper() for c in password_list)
-    has_symbol = any(not c.isalnum() for c in password_list)
-    has_number = any(c.isdigit() for c in password_list)
-    
-    replacements = []
-    if use_lowercase and not has_lowercase: replacements.append(secrets.choice(Lowercase))
-    if use_uppercase and not has_uppercase: replacements.append(secrets.choice(Uppercase))
-    if use_symbol and not has_symbol: replacements.append(secrets.choice(Symbol))
-    if use_number and not has_number: replacements.append(secrets.choice(Number))
-
-    for i, replacement in enumerate(replacements):
-        if i < len(password_list):
-            password_list[i] = replacement
-    
+    passwordSET = set()
     available_chars = ""
-    if use_lowercase: available_chars += Lowercase
-    if use_uppercase: available_chars += Uppercase
-    if use_symbol: available_chars += Symbol
-    if use_number: available_chars += Number
-
-    seen = set()
-    for i in range(len(password_list)):
-        if password_list[i] in seen:
-            for char in available_chars:
-                if char not in seen:
-                    password_list[i] = char
-                    break
-        seen.add(password_list[i])
     
-    return ''.join(password_list)
+    if UseLowercase: available_chars += Lowercase
+    if UseUppercase: available_chars += Uppercase
+    if UseSymbol: available_chars += Symbol
+    if UseNumber: available_chars += Number
+
+    if UseLowercase: passwordSET.add(secrets.choice(Lowercase))
+    if UseUppercase: passwordSET.add(secrets.choice(Uppercase))
+    if UseSymbol:    passwordSET.add(secrets.choice(Symbol))
+    if UseNumber:    passwordSET.add(secrets.choice(Number))
+
+    while len(passwordSET) < Length:
+        Newchar = secrets.choice(available_chars)
+        passwordSET.add(Newchar)
+
+    Passwordchars = list(passwordSET)
+    secrets.SystemRandom().shuffle(Passwordchars)
+    return ''.join(Passwordchars)
 
 print ("The password generator")
 while True:
@@ -68,7 +49,6 @@ while True:
     else:
         UseLowercase, UseUppercase, UseSymbol, UseNumber = User_preferences()
         password = Passgen(Length, UseLowercase, UseUppercase, UseSymbol, UseNumber, Lowercase, Uppercase, Symbol, Number)
-        password = Improve_password(password, UseLowercase, UseUppercase, UseSymbol, UseNumber, Lowercase, Uppercase, Symbol, Number)
         print("Generated Password: " + password)
 
     print("Do you want to generate another password? (yes/no)")
